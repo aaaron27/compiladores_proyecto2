75ed6f5129724f89f839727a7f46fc40e058623f
aaaron27 <a.perez.1@estudiantec.cr>
2026-01-14 22:07:41 -0600

    save #2

--- app/src/main/java/org/analizadorLexico/ast/Loop_statement_node.java
+++ app/src/main/java/org/analizadorLexico/ast/Loop_statement_node.java
@@ -1,5 +1,13 @@
 package org.analizadorLexico.ast;
 
 public class Loop_statement_node extends NodoAST {
-
+    public Loop_statement_node(NodoAST sentencias, NodoAST condicion) {
+        super();
+        if (sentencias != null) {
+            this.agregarHijo(sentencias);
+        }
+        if (condicion != null) {
+            this.agregarHijo(condicion);
+        }
+    }
 }
